efc6dea46e618134b2650b3122c67a23d7b190a2
perezsergioace <perez.sergio.ace@gmail.com>
2020-01-31 11:43:34 -0500

    setup the authenticate-middleware file

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,24 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const jwt = require('jsonwebtoken');
+
+const { jwtSecret } = require('../config/secrets');
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  const { authorization } = req.headers;
+
+  if (authorization) {
+    jwt.verify(authorization, jwtSecret, (error, token) => {
+      if (error) {
+        res.status(401).json({ error: "invalid token" })
+      } else {
+        req.user = { username: token.username }
+
+        next();
+      }
+    })
+  } else {
+    res.status(401).json({ you: 'shall not pass!' });
+  }
 };
